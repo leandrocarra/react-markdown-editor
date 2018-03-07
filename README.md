bd7afb0ad99d6e0bfa699381c3a391952f254d03
Leandro <leandrocarra@Leandros-MacBook-Air.local>
2018-03-07 16:35:03 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# react-markdown-editor
+# Markdown editor
+
---
+++ src/app.js
@@ -0,0 +1,27 @@
+'use script'
+
+import React, { Component } from 'react'
+import MarkdownEditor from './markdown-editor'
+
+import './css/style.css'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = { value: '' }
+    this.handleChange = (e) => {
+      this.setState({ value: e.target.value })
+    }
+  }
+
+  render () {
+    return (
+      <MarkdownEditor
+        value={this.state.value}
+        handleChange={this.handleChange}
+      />
+    )
+  }
+}
+
+export default App
